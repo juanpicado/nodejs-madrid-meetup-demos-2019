d7198d7ff965f00af20183669a15d620ab78ecb5
Juan Picado @jotadeveloper <juanpicado19@gmail.com>
2019-02-27 22:13:22 +0100

    agrega plugin ejemplo

---
+++ plugins/auth-endpoint/build/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+class AuthEndpoint {
+  constructor(config, options) {
+    this.config = config;
+    this.logger = options.logger;
+    this.realConfig = options.config;
+  }
+
+  authenticate(user, password, callback) {
+    this.logger.info("logging a user");
+    this.logger.info({
+      user,
+      password
+    }, "logging a user: @{user}|@{password}");
+    callback(null, ['$all']);
+  }
+
+  adduser(user, password, cb) {
+    this.logger.info("adding a user");
+    this.logger.info({
+      user,
+      password
+    }, "adding a user: @{user}|@{password}");
+    cb(null, true);
+  }
+
+}
+
+exports.default = AuthEndpoint;
